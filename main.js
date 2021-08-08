c84fed9964491603855a2a4a6c2d6753d706f9e5
ShashwatRoyIT <63514592+ShashwatRoyIT@users.noreply.github.com>
2021-08-09 00:53:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+const inputTxt = document.querySelector('.inputTxt');
+var button = document.querySelector('.btn');
+const showData = document.querySelector('.showData');
+
+
+const API_KEY = "cb6e60b3bd56de62b892564526947ea8";
+
+
+button.addEventListener('click', () => {
+
+    const cityInput = inputTxt.Value;
+    console.log(cityInput);
+
+
+    
+
+})
